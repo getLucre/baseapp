c94a8f0b5d19187a5c4fec2db95b90757509d935
LucreAdmin <developer@getlucre.xyz>
2022-08-11 14:02:29 +0100

    added address section to footer

--- src/theme/Footer/index.tsx
+++ src/theme/Footer/index.tsx
@@ -1,9 +1,11 @@
+import Address from "@site/src/components/Address";
 import Footer from "@theme-original/Footer";
 import React from "react";
 
 export default function FooterWrapper(props: any) {
   return (
     <>
+      <Address />
       <Footer {...props} />
     </>
   );
